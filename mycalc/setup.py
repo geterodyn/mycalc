b07970bf91b25012275d5a0db7e9db8f8770101b
Sergey Lunkov <geterodyn@gmail.com>
2020-01-11 23:09:42 +0700

    add setup

---
+++ mycalc/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mycalc',
+    version='0.0.1',
+    description='some arithmetics just for fun',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Sergey Lunkov',
+    author_email='geterodyn@gmail.com',
+    url='https://github.com/geterodyn/mycalc',
+)
